9eea1ce5bcd28515bfe33274b291d48e68a47a26
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-04-15 23:34:02 +0100

    Moving parse logic to a separate file

---
+++ tp1/TheaterBite/app/src/main/java/org/feup/carlosverissimo3001/theaterbite/models/Parser.kt
@@ -0,0 +1,34 @@
+package org.feup.carlosverissimo3001.theaterbite.models
+
+import org.json.JSONObject
+
+/**
+ * Class responsible for parsing JSON objects into model objects
+ */
+object Parser {
+    /**
+     * Parses a JSON object into a Product object
+     * @param jsonObject JSON object to parse
+     * @return Product object
+     */
+    fun parseProduct(jsonObject: JSONObject): Product {
+        val name = jsonObject.optString("itemname", "")
+        val price = jsonObject.optDouble("price", 0.0)
+        val quantity = jsonObject.optInt("quantity", 0)
+        return Product(name, price, quantity)
+    }
+
+    /**
+     * Parses a JSON object into a Voucher object
+     * @param jsonObject JSON object to parse
+     * @return Voucher object
+     */
+    fun parseVoucher (jsonObject: JSONObject): Voucher {
+        return Voucher(
+            jsonObject.getString("voucherid"),
+            jsonObject.getString("vouchertype"),
+            jsonObject.getBoolean("isUsed"),
+            jsonObject.getString("userid"),
+        )
+    }
+}
---
+++ tp1/TheaterBite/app/src/main/java/org/feup/carlosverissimo3001/theaterbite/models/ShowDate.kt
@@ -0,0 +1,15 @@
+package org.feup.carlosverissimo3001.theaterbite.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Data class representing a show date
+ * @property date date of the show
+ * @property showdateid id of the show date
+ */
+@Parcelize
+data class ShowDate(
+    val date: String,
+    val showdateid: Int
+) : Parcelable
