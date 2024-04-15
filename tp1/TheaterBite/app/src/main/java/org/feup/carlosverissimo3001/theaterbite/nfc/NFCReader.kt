d400817a2966594e5db12611c88d68d5e51087fa
Carlos Verissimo <carlosverissimo3001@gmail.com>
2024-04-15 23:33:35 +0100

    Moving nfc to seperate folder

--- tp1/TheaterBite/app/src/main/java/org/feup/carlosverissimo3001/theaterbite/nfc/NFCReader.kt
+++ tp1/TheaterBite/app/src/main/java/org/feup/carlosverissimo3001/theaterbite/nfc/NFCReader.kt
@@ -1,9 +1,12 @@
-package org.feup.carlosverissimo3001.theaterbite
+package org.feup.carlosverissimo3001.theaterbite.nfc
 
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.IsoDep
 import android.util.Log
+import android.widget.Toast
+import org.feup.carlosverissimo3001.theaterbite.byteArrayToHex
+import org.feup.carlosverissimo3001.theaterbite.hexStringToByteArray
 import java.io.IOException
 
 private const val CARD_AID = "F010203040"
@@ -12,23 +15,34 @@ private val RES_OK_SW = hexStringToByteArray("9000")
 
 class NFCReader(private val listener: (Int, ByteArray)->Unit) : NfcAdapter.ReaderCallback {
     override fun onTagDiscovered(tag: Tag) {
-        val isoDep = IsoDep.get(tag)                    // Android smartcard reader emulator
+        val isoDep = IsoDep.get(tag)                        // Android smart-card reader emulator
         if (isoDep != null) {
             try {
                 isoDep.connect()                            // establish a connection with the card and send 'select aid' command
                 val result = isoDep.transceive(hexStringToByteArray(CMD_SEL_AID + String.format("%02X", CARD_AID.length/2) + CARD_AID))
                 val rLen = result.size
                 val status = byteArrayOf(result[rLen-2], result[rLen-1])
-                println("Status: ${byteArrayToHex(status)}")
+                Log.d("CardReader", "Status: ${byteArrayToHex(status)}")
                 if (RES_OK_SW.contentEquals(status)) {
                     listener(
                         result[0].toInt(),
                         result.sliceArray(1..rLen - 3)
-                    )      // received a key (type == 1) or a list (type == 2)
+                    )
                 }
             } catch (e: IOException) {
-                Log.e("CardReader", "Error communicating with card: $e")
+                handleCommunicationError("Error communicating with card: ${e.message}")
+            }
+            finally {
+                try {
+                    isoDep.close()
+                } catch (e: IOException) {
+                    Log.e("CardReader", "Error closing connection: ${e.message}")
+                }
             }
         }
+    }
+
+    private fun handleCommunicationError(errorMessage: String) {
+        Toast.makeText(null, errorMessage, Toast.LENGTH_LONG).show()
     }
 }
