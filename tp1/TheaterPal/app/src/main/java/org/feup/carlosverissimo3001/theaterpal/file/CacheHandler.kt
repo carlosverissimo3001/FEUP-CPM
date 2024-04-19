e43785492426755de1ef5ffe68f2461ef31d59b3
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2024-04-19 23:45:24 +0100

    Added method to save vouchers to cache

--- tp1/TheaterPal/app/src/main/java/org/feup/carlosverissimo3001/theaterpal/file/CacheHandler.kt
+++ tp1/TheaterPal/app/src/main/java/org/feup/carlosverissimo3001/theaterpal/file/CacheHandler.kt
@@ -9,7 +9,9 @@ import org.feup.carlosverissimo3001.theaterpal.models.Parser.parseShow
 import org.feup.carlosverissimo3001.theaterpal.models.Parser.parseTicket
 import org.feup.carlosverissimo3001.theaterpal.models.Parser.showsToJson
 import org.feup.carlosverissimo3001.theaterpal.models.Parser.ticketsToJson
+import org.feup.carlosverissimo3001.theaterpal.models.Parser.vouchersToJson
 import org.feup.carlosverissimo3001.theaterpal.models.Ticket
+import org.feup.carlosverissimo3001.theaterpal.models.Voucher
 import org.feup.carlosverissimo3001.theaterpal.models.show.Show
 import org.json.JSONArray
 import org.json.JSONObject
@@ -107,22 +109,8 @@ fun areShowsStoredInCache(context: Context): Boolean {
 }
 
 
-fun loadImageFromCache(filename: String, context: Context): Bitmap? {
-    val cacheDir = context.cacheDir
-    val imagesCacheDir = File(cacheDir, "images")
 
-    if (!imagesCacheDir.exists()) {
-        return null
-    }
-
-    val imageFile = File(imagesCacheDir, filename)
-    return BitmapFactory.decodeFile(imageFile.absolutePath)
-}
-
-fun decodeBase64ToBitmap(base64String: String): Bitmap? {
-    val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
-    return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
-}
+/**** TICKETS ****/
 
 /**
  * When the user purchases tickets, the server will return a list of tickets.
@@ -177,6 +165,14 @@ fun appendTicketsToCache(tickets: JSONArray, context: Context, isSuccess: (Boole
     }
 }
 
+/**
+ * Save a list of tickets to the cache
+ * @param tickets List of tickets to save to the cache
+ * @param context Context of the application
+ * @param isSuccess Callback function to be called when the operation is done
+ * @see appendTicketsToCache
+ * @see Ticket
+ */
 fun saveTicketsToCache(tickets: List<Ticket>, context: Context, isSuccess: (Boolean) -> Unit){
     val cacheDir = context.cacheDir
     val ticketsCacheDir = File(cacheDir, "tickets")
@@ -201,6 +197,11 @@ fun saveTicketsToCache(tickets: List<Ticket>, context: Context, isSuccess: (Bool
     }
 }
 
+/**
+ * Check if the tickets are stored in the cache
+ * @param context Context of the application
+ * @return True if the tickets are stored in the cache, false otherwise
+ */
 fun areTicketsStoredInCache(context: Context): Boolean {
     val cacheDir = context.cacheDir
     val ticketsCacheDir = File(cacheDir, "tickets")
@@ -208,6 +209,12 @@ fun areTicketsStoredInCache(context: Context): Boolean {
     return ticketsCacheDir.exists()
 }
 
+/**
+ * Load the tickets from the cache
+ * @param context Context of the application
+ * @param callback Callback function to be called when the tickets are loaded
+ * @see Ticket
+ */
 fun loadTicketsFromCache(context: Context, callback: (List<Ticket>) -> Unit) {
     val cacheDir = context.cacheDir
     val ticketsCacheDir = File(cacheDir, "tickets")
@@ -234,4 +241,53 @@ fun loadTicketsFromCache(context: Context, callback: (List<Ticket>) -> Unit) {
     }
 
     callback(tickets)
+}
+
+/**** TICKETS ****/
+
+/**** VOUCHERS ****/
+
+fun saveVouchersToCache(vouchers: List<Voucher>, context: Context, isSuccess: (Boolean) -> Unit){
+    val cacheDir = context.cacheDir
+    val vouchersCacheDir = File(cacheDir, "vouchers")
+    val vouchersString = vouchersToJson(vouchers)
+
+    if(!vouchersCacheDir.exists()){
+        vouchersCacheDir.mkdirs()
+    }
+
+    val vouchersFile = File(vouchersCacheDir, "vouchers.json")
+
+    try {
+        val stream = FileOutputStream(vouchersFile)
+        stream.write(vouchersString.toByteArray())
+        stream.close()
+        isSuccess(true)
+    }
+    catch (e: Exception) {
+        e.printStackTrace()
+        isSuccess(false)
+    }
+}
+
+/**** VOUCHERS ****/
+
+
+/**** OTHER ****/
+
+fun loadImageFromCache(filename: String, context: Context): Bitmap? {
+    val cacheDir = context.cacheDir
+    val imagesCacheDir = File(cacheDir, "images")
+
+    if (!imagesCacheDir.exists()) {
+        return null
+    }
+
+    val imageFile = File(imagesCacheDir, filename)
+    return BitmapFactory.decodeFile(imageFile.absolutePath)
+}
+
+fun decodeBase64ToBitmap(base64String: String): Bitmap? {
+    val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+    return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
 }
